ce4a5b193dc7b1a6a1b66c098f538a5ed0cc14b0
Philip Nelson <PhilipNelson5@gmail.com>
2018-09-11 21:21:26 -0600

    finished 2

--- hw2/2-derivativeApproximation/main.cpp
+++ hw2/2-derivativeApproximation/main.cpp
@@ -4,12 +4,17 @@
 #include <iomanip>
 #include <iostream>
 
+double poly(double x)
+{
+  return x*x*x + 2*x*x + 10*x + 7;
+}
+
 int main()
 {
-  auto x = 1.0;
+  auto x = 3.0;
   auto h = 1.0;
-  auto value = cos(x);
-  auto f = sin;
+  auto value = 3*x*x + 4*x + 10;
+  auto f = poly;
   auto approx = 0.0;
   auto error_abs = 0.0;
   auto error_rel = 0.0;
