2c7fe81d678a4193bbbb89913a860e2453c5b9cf
Philip Nelson <PhilipNelson5@gmail.com>
2018-09-13 23:18:50 -0600

    finished 6

---
+++ hw2/6-newton/newton.hpp
@@ -0,0 +1,33 @@
+#ifndef NEWTON_HPP
+#define NEWTON_HPP
+
+#include <cmath>
+#include <iostream>
+
+/**
+ * @tparam T       The type of x0 and tolerance
+ * @tparam F       A function of type T(T)
+ * @tparam Fprime  A function of type T(T)
+ * @param x0       The starting point
+ * @param tol      The Tolerance
+ * @param MAX_ITER The maximum iterations
+ */
+template <typename T, typename F, typename Fprime>
+T root_finder_newton(F f, Fprime fprime, T x0, T tol, const int MAX_ITER = 100)
+{
+  T x1;
+
+  for (auto i = 0; i < MAX_ITER; ++i)
+  {
+    x1 = x0 - f(x0) / fprime(x0);
+    if (std::abs(x1 - x0) < tol * std::abs(x1))
+    {
+      break;
+    }
+    x0 = x1;
+  }
+
+  return x1;
+}
+
+#endif
